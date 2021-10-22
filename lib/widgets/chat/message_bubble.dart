b2fb36852ea9b719b686ea51b9c683cc85029fe6
Nico Proto <nicolasproto@gmail.com>
2021-10-22 14:53:35 +0200

    Improving the chat bubbles

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -2,17 +2,27 @@ import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
   final String message;
+  final bool isMe;
+  final Key key;
 
-  MessageBubble(this.message);
+  MessageBubble(this.message, this.isMe, {this.key});
 
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: [
         Container(
           decoration: BoxDecoration(
-            color: Theme.of(context).accentColor,
-            borderRadius: BorderRadius.circular(12),
+            color: isMe
+            ? Colors.grey[300]
+            : Theme.of(context).accentColor,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(12),
+              topRight: Radius.circular(12),
+              bottomLeft: !isMe ? Radius.circular(0) : Radius.circular(12),
+              bottomRight: !isMe ? Radius.circular(12) : Radius.circular(0),
+            ),
           ),
           width: 140,
           padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
@@ -20,7 +30,9 @@ class MessageBubble extends StatelessWidget {
           child: Text(
             message,
             style: TextStyle(
-              color: Theme.of(context).accentTextTheme.headline1.color,
+              color: isMe
+              ? Colors.black
+              : Theme.of(context).accentTextTheme.headline1.color,
             ),
           ),
         ),
--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -6,19 +7,38 @@ import '/widgets/chat/message_bubble.dart';
 class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: Firestore.instance.collection('chat').orderBy('createdAt', descending: true).snapshots(),
-      builder: (ctx, chatSnapshot) {
-        if (chatSnapshot.connectionState == ConnectionState.waiting) {
+    return FutureBuilder(
+      future: FirebaseAuth.instance.currentUser(),
+      builder: (ctx, futureSnapshot) {
+        if (futureSnapshot.connectionState == ConnectionState.waiting) {
           return Center(child: CircularProgressIndicator(),);
         }
-        final chatDocs = chatSnapshot.data.documents;
-        return ListView.builder(
-          reverse: true,
-          itemBuilder: (ctx, index) => MessageBubble(chatDocs[index]['text']),
-          itemCount: chatDocs.length,
-        );
 
+        return StreamBuilder(
+          stream: Firestore.instance
+            .collection('chat')
+            .orderBy(
+              'createdAt',
+              descending: true
+            )
+            .snapshots(),
+          builder: (ctx, chatSnapshot) {
+            if (chatSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator(),);
+            }
+            final chatDocs = chatSnapshot.data.documents;
+
+            return ListView.builder(
+              reverse: true,
+              itemBuilder: (ctx, index) => MessageBubble(
+                chatDocs[index]['text'],
+                chatDocs[index]['userId'] == futureSnapshot.data.uid,
+                key: ValueKey(chatDocs[index].documentID),
+              ),
+              itemCount: chatDocs.length,
+            );
+          }
+        );
       },
     );
   }
