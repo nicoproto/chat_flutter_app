69ffda769fcff2ddb7b09341880f50f23e91c551
Nico Proto <nicolasproto@gmail.com>
2021-10-22 14:33:12 +0200

    Creating a basic Chat Bubble widget

---
+++ lib/widgets/chat/message_bubble.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MessageBubble extends StatelessWidget {
+  final String message;
+
+  MessageBubble(this.message);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).accentColor,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          width: 140,
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+          margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+          child: Text(
+            message,
+            style: TextStyle(
+              color: Theme.of(context).accentTextTheme.headline1.color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '/widgets/chat/message_bubble.dart';
+
 class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -13,7 +15,7 @@ class Messages extends StatelessWidget {
         final chatDocs = chatSnapshot.data.documents;
         return ListView.builder(
           reverse: true,
-          itemBuilder: (ctx, index) => Text(chatDocs[index]['text']),
+          itemBuilder: (ctx, index) => MessageBubble(chatDocs[index]['text']),
           itemCount: chatDocs.length,
         );
 
