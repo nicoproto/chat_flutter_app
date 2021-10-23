7350ced53e56322ba4f72e1cb2c6db02365350ff
Nico Proto <nicolasproto@gmail.com>
2021-10-23 18:27:13 +0200

    Displaying user names

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -1,11 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
   final String message;
+  final String username;
   final bool isMe;
   final Key key;
 
-  MessageBubble(this.message, this.isMe, {this.key});
+  MessageBubble(this.message, this.username,this.isMe, {this.key});
 
   @override
   Widget build(BuildContext context) {
@@ -27,13 +29,28 @@ class MessageBubble extends StatelessWidget {
           width: 140,
           padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
           margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-          child: Text(
-            message,
-            style: TextStyle(
-              color: isMe
-              ? Colors.black
-              : Theme.of(context).accentTextTheme.headline1.color,
-            ),
+          child: Column(
+            crossAxisAlignment: isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+            children: [
+              Text(
+                username,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: isMe
+                        ? Colors.black
+                        : Theme.of(context).accentTextTheme.headline1.color,
+                ),
+              ),
+              Text(
+                message,
+                style: TextStyle(
+                  color: isMe
+                  ? Colors.black
+                  : Theme.of(context).accentTextTheme.headline1.color,
+                ),
+                textAlign: isMe ? TextAlign.end : TextAlign.start,
+              ),
+            ],
           ),
         ),
       ],
